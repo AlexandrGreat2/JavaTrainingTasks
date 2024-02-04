6c404b255ed61c925cfb59aa6ea6159d47513fb7
Timal <alexandrfj5@gmail.com>
2024-02-05 01:34:11 +0200

    distance2D method fix

--- Module8/src/main/java/org/example/ShapeHelper.java
+++ Module8/src/main/java/org/example/ShapeHelper.java
@@ -9,6 +9,8 @@ public final class ShapeHelper {
         if (point1.length != 2 || point2.length != 2) {
             throw new Exception("Wrong point coordinates data. Should be use 2d points with 2 coordinates X and Y");
         }
-        return Math.sqrt( Math.pow(point1[0],2) + Math.pow(point1[1],2) );
+        return Math.sqrt(
+                Math.pow(point1[0]-point2[0],2) + Math.pow(point1[1]-point2[1],2)
+        );
     }
 }
