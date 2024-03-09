cac2859a95347bae520b2455bbf4d72921fd38fa
Timal <alexandrfj5@gmail.com>
2024-03-09 19:05:15 +0200

    Module10 fix Module10 add pom Module10

---
+++ Module10/src/main/java/org/example/PhoneNumberValidator.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumberValidator {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        validatePhoneNumbers("F:\\java\\javaTrainingTasks\\JavaTrainingTasks\\Module10\\src\\main\\java\\org\\example\\file.txt");
+    }
+
+
+    private static void validatePhoneNumbers(String fileName) {
+        //FileReader filereader = new FileReader(fileName);
+        //BufferedReader reader = new BufferedReader(filereader);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (isValidPhoneNumber(line)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static boolean isValidPhoneNumber(String phoneNumber) {
+        String regex = "(\\d{3}-\\d{3}-\\d{4})|(\\(\\d{3}\\) \\d{3}-\\d{4})";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+
+
+}
---
+++ Module10/src/main/java/org/example/UserToJson.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserToJson {
+    public static void main(String[] args) {
+        usersToJson("F:\\java\\javaTrainingTasks\\JavaTrainingTasks\\Module10\\src\\main\\java\\org\\example\\file2.txt", "F:\\java\\javaTrainingTasks\\JavaTrainingTasks\\Module10\\src\\main\\java\\org\\example\\user.json");
+    }
+
+    private static void usersToJson(String inputFileName, String outputFileName) {
+        List<User> users = readUsersFromFile(inputFileName);
+        writeUsersToJsonFile(users, outputFileName);
+    }
+
+    private static List<User> readUsersFromFile(String fileName) {
+        List<User> users = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            // Пропустимо перший рядок, який є заголовком
+            reader.readLine();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(" ");
+                String name = data[0];
+                int age = Integer.parseInt(data[1]);
+                users.add(new User(name, age));
+            }
+        } catch (Exception ignored) {
+
+        }
+
+        return users;
+    }
+
+    private static void writeUsersToJsonFile(List<User> users, String outputFileName) {
+        try (FileWriter writer = new FileWriter(outputFileName)) {
+            writer.write("[\n");
+            int iteration = 0;
+            String commaText = ",%n";
+            for (User user : users) {
+                if(iteration != 0) {
+                    writer.write(String.format(",%n"));
+                }
+                iteration++;
+                writer.write(String.format("    {\n        \"name\": \"%s\",\n        \"age\": %d\n    }",
+                        user.getName(), user.getAge()));
+            }
+
+            writer.write("\n]");
+        } catch (Exception ignored) {
+
+        }
+    }
+}
+
+class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
