f1cacd7809ccceb556876686fc957a1efb731130
Timal <alexandrfj5@gmail.com>
2024-03-10 02:11:51 +0200

    Module12 try to fix

--- Module12/src/main/java/org/example/FizzBuzz.java
+++ Module12/src/main/java/org/example/FizzBuzz.java
@@ -6,48 +6,72 @@ import java.util.concurrent.TimeUnit;
 
 public class FizzBuzz {
     private int n;
-    private LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+    private int currentNumber = 1;
+    private Object lock = new Object();
 
     public FizzBuzz(int n) {
         this.n = n;
     }
 
     public void fizz() {
-        for (int i = 1; i <= n; i++) {
-            if (i % 5 == 0) {
-                queue.offer("fizz");
+        while (currentNumber <= n) {
+            synchronized (lock) {
+                if (currentNumber % 3 == 0 && currentNumber % 5 != 0) {
+                    System.out.println("fizz");
+                    currentNumber++;
+                    lock.notifyAll();
+                }
             }
         }
     }
 
     public void buzz() {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0) {
-                queue.offer("buzz");
+        while (currentNumber <= n) {
+            synchronized (lock) {
+                if (currentNumber % 5 == 0 && currentNumber % 3 != 0) {
+                    System.out.println("buzz");
+                    currentNumber++;
+                    lock.notifyAll();
+                }
             }
         }
     }
 
     public void fizzbuzz() {
-        for (int i = 1; i <= n; i++) {
-            if (i % 15 == 0) {
-                queue.offer("fizzbuzz");
+        while (currentNumber <= n) {
+            synchronized (lock) {
+                if (currentNumber % 3 == 0 && currentNumber % 5 == 0) {
+                    System.out.println("fizzbuzz");
+                    currentNumber++;
+                    lock.notifyAll();
+                }
             }
         }
     }
 
     public void number() {
-        for (int i = 1; i <= n; i++) {
-            try {
-                String output = queue.poll(100, TimeUnit.MILLISECONDS); // Wait for 100 milliseconds
-                if (Objects.equals(output, "fizz") || Objects.equals(output, "buzz") || Objects.equals(output, "fizzbuzz")) {
-                    System.out.println(output);
-                } else {
-                    System.out.println(i);
+        while (currentNumber <= n) {
+            synchronized (lock) {
+                if (currentNumber % 3 != 0 && currentNumber % 5 != 0) {
+                    System.out.println(currentNumber);
+                    currentNumber++;
+                    lock.notifyAll();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+
+        Thread threadA = new Thread(fizzBuzz::fizz);
+        Thread threadB = new Thread(fizzBuzz::buzz);
+        Thread threadC = new Thread(fizzBuzz::fizzbuzz);
+        Thread threadD = new Thread(fizzBuzz::number);
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
     }
 }
